0a7db9cc4b3a3f57174b6b447bcc484b0886dc87
Daniel Sanz <daniel.sanz@liferay.com>
2019-05-14 11:49:02 +0200

    Add a soy portlet names "mytest01" as it was created from blade template by running "blade create -t soy-portlet mytest01"

---
+++ modules/mytest01/src/main/java/mytest01/constants/Mytest01PortletKeys.java
@@ -0,0 +1,10 @@
+package mytest01.constants;
+
+/**
+ * @author dsanz
+ */
+public class Mytest01PortletKeys {
+
+	public static final String Mytest01 = "mytest01";
+
+}
---
+++ modules/mytest01/src/main/java/mytest01/portlet/Mytest01SoyPortletRegister.java
@@ -0,0 +1,31 @@
+package mytest01.portlet;
+
+import mytest01.constants.Mytest01PortletKeys;
+
+import com.liferay.portal.portlet.bridge.soy.SoyPortletRegister;
+
+import java.io.IOException;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author dsanz
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=false",
+		"com.liferay.portlet.single-page-application=false",
+		"javax.portlet.display-name=mytest01 Portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=View",
+		"javax.portlet.name=" + Mytest01PortletKeys.Mytest01,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	}
+)
+public class Mytest01SoyPortletRegister implements SoyPortletRegister {
+}
